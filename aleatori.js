2ae78c1c74adffecb8453699cf12210f4795388e
sethei <sethei86239@gmail.com>
2018-10-10 00:24:09 +0200

    tercera versio

--- aleatori.js
+++ aleatori.js
@@ -1,11 +1,13 @@
 function numeroAleatori(){
     var numero = parseInt(prompt("Introdueix el numero a buscar"));
     var tempsT = (Date.now() % 1000) / 1000;
-    for (i=0;i<500000;i++){
-        var aleatori=Math.random()*500000;
+    for (i=0;i<700000;i++){
+        var aleatori=Math.random()*700000;
+	if (i%2==0){
         if (numero=aleatori){
             alert("Ha trigat "+tempsT);
             break;
         }
+	}
     }
 }
