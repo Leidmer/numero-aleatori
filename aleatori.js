2881f786229d87f79e12797a9599a830c304965c
sethei <sethei86239@gmail.com>
2018-10-25 11:13:39 +0200

    Prova

--- aleatori.js
+++ aleatori.js
@@ -1,5 +1,5 @@
 function numeroAleatori(){
-    var numero = parseInt(prompt("Introdueix el numero a buscar"));
+    var numero = parseInt(prompt("Introdueix el numero a buscar2"));
     var tempsT = (Date.now() % 1000) / 1000;
     for (i=0;i<700000;i++){
         var aleatori=Math.random()*700000;
